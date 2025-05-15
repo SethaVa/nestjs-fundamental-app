4673b0b95d9835546493e6bdd8605ba441ecc5b8
Setha Va <vasetha1@gmail.com>
2025-05-16 00:29:18 +0700

    Set up hot reload

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { SeedService } from './seed/seed.service';
 import { ConfigService } from '@nestjs/config';
 
+declare interface NodeModule {
+    hot?: {
+        accept: () => void;
+        dispose: (callback: () => void) => void;
+    };
+}
+
+declare const module: NodeModule;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
@@ -11,5 +20,12 @@ async function bootstrap() {
     await seedService.seed();
     const configService = app.get(ConfigService);
     await app.listen(configService.get<number>('port') ?? 3000);
+
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => {
+            void app.close();
+        });
+    }
 }
 bootstrap();
